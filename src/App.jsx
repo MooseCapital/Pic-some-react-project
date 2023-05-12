e5145207a3a9f0401af03e32f4ff624b06fcaa88
Moose Capital <48196495+MooseCapital@users.noreply.github.com>
2023-05-12 17:06:21 -0500

    project save

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+//image import
+//const viteLogo = new URL('/vite.svg', import.meta.url).href
+
+function App() {
+
+
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
